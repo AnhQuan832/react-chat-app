c6117f1869ce825da2a149ca7d689b1555b21120
Anh Quan <quandang123654@gmail.com>
2024-10-24 18:15:39 +0700

    feat: add get user info by jwt

--- client/src/features/chat/chat.tsx
+++ client/src/features/chat/chat.tsx
@@ -1,5 +1,17 @@
+import { useAppSelector } from "@/app/hook";
 import React from "react";
 
 export const Chat = () => {
-  return <div>Chat</div>;
+  const userInfo = useAppSelector((state) => state.user);
+  return (
+    <div>
+      <p>ID: {userInfo.user.id}</p>
+      <p>Name: {userInfo.user.name}</p>
+      <p>Email: {userInfo.user.email}</p>
+      {userInfo.user.image && (
+        <img src={userInfo.user.image} alt="User Image" />
+      )}
+      <p>Token: {userInfo.user.token}</p>
+    </div>
+  );
 };
